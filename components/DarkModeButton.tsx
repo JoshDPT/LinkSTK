207e1c05a91f1ecfaf033bd8c67c8c37fbbd8ea4
JoshDPT <jjewcu@gmail.com>
2023-03-06 13:13:51 -0500

    tweak button

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -27,7 +27,7 @@ export default function DarkModeButton () {
           />
       ) : (
         <MoonIcon
-          className="h-8 w-8 cursor-pointer text-gray-900"
+          className="h-8 w-8 cursor-pointer text-violet-500"
           onClick={()=> setTheme('dark')}
         />
       )}
